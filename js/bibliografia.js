d64081997fbcedf06e0afcbbbfa3bae0f8230b5c
Juancolque04 <jdiazcolque04@gmail.com>
2024-10-21 23:43:41 -0300

    c1

--- js/bibliografia.js
+++ js/bibliografia.js
@@ -15,16 +15,29 @@ const section = uploadForm.getAttribute('data-section');
 // Loader
 function showLoader() {
     document.body.classList.add('loading');
-  }
-  
-  function hideLoader() {
+}
+
+function hideLoader() {
     document.body.classList.remove('loading');
     document.body.classList.add('loaded');
-  }
-  
+}
+
 async function initApp() {
-    await checkAuth(); // Comprobar autenticación
-    loadFiles(); // Cargar archivos subidos
+    showLoader();
+    try {
+        await checkAuth();
+        await Promise.all([loadFiles()]);
+        hideLoader();
+    } catch (error) {
+        console.error('Error durante la inicialización:', error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Ocurrió un error al cargar la aplicación. Por favor, intenta de nuevo más tarde.',
+            confirmButtonText: 'Aceptar'
+        });
+        hideLoader();
+    }
 }
 
 // Actualizar el nombre del archivo seleccionado
